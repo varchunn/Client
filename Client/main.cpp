9bf8cd0c920e69b023fcb09a2aae19852b57ea1c
varchunn <varvaram186@gmail.com>
2025-02-24 03:36:13 +0200

    Initial Commit

---
+++ Client/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Client
+//
+//  Created by Варя Мінєнкова on 24.02.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
